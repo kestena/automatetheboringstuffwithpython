dc802c357f7a28d9f6ef92b16a3bdb1f5cb5c471
kestena <37227049+kestena@users.noreply.github.com>
2018-09-30 18:57:17 +0200

    Update working_with_lists_projects.py

--- working_with_lists_projects.py
+++ working_with_lists_projects.py
@@ -1,1 +1,17 @@
+grid = [['.', '.', '.', '.', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['.', 'O', 'O', 'O', 'O', 'O'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.']]
 
+x = len(grid[0]) #If we increase the grid with another row(s) this will adapt
+y = len(grid)    #itself instead of hardcoding the ranges.
+
+for i in range (x):
+    for j in range (y):
+        print (grid[j][i], end="")     
+    print()
